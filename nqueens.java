d70c5a2c165e3578991bb5e7ce7cc53c603d6a70
Abhishek kumar kar <72333625+SP1RITX@users.noreply.github.com>
2024-04-11 03:50:24 +0530

    Create nqueens.java

---
+++ nqueens.java
@@ -0,0 +1,68 @@
+public class nqueens {
+    // FUNCTION  TO CHECK IF THE QUEENS ARE ATTACKING EACH OTHER OR NOT
+    public static boolean isSafe(char board[][], int row, int col){
+      //vertical up
+       for(int i=row-1;i>=0;i--){
+        if(board[i][col]=='Q'){
+            return false;
+        }
+       }
+      //diag left up
+       for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+       if(board[i][j]=='Q'){
+        return false;
+       }
+       }
+      //diag right up
+      for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
+        if(board[i][j]=='Q'){
+            return false;
+        }
+      }
+    return true;}
+
+    // FUNCTION TO PUT N-QUEENS ON THE N*N BOARD
+    public static void nQueens(char board[][],int row){
+        //base
+        if(row == board.length){
+            printBoard(board);
+            count++;
+            return;
+        }
+        //column loop 
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board, row, j)){
+                board[row][j]='Q';
+                nQueens(board,row+1);
+                board[row][j]='.';
+            }
+            
+        }
+    }
+//FUNCTION TO PRINT THE CHESS BOARD
+    public static void printBoard(char board[][]){
+        System.out.println("--------chess--------");
+        for(int i =0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+//MAIN FUNCTION
+ static int count =0;
+    public static void main(String args[]){
+        int n = 3;
+        char board[][]= new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+            nQueens(board,0);
+            System.out.println("TOTAL WAYS TO SOLVE N-QUEENS : "+ count);
+    }
+}
